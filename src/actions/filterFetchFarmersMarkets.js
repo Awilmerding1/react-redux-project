6b74d39f20be15fa21f0fac703228992edb020e3
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-13 17:36:29 -0500

    working on filters

--- src/actions/filterFetchFarmersMarkets.js
+++ src/actions/filterFetchFarmersMarkets.js
@@ -3,6 +3,7 @@ import fetch from 'isomorphic-fetch';
 export function filterFetchFarmersMarkets(data) {
   let dataValues = Object.values(data)
   dataValues.shift()
+  let filteredMarkets = []
   if (data.search !== "") {
   return (dispatch) => {
 	  dispatch({ type: 'LOADING_MARKETS' })
@@ -17,6 +18,7 @@ export function filterFetchFarmersMarkets(data) {
 	  dispatch({ type: 'LOADING_MARKETS' })
   return fetch(`https://data.cityofnewyork.us/resource/94pk-v63f.json`)
   .then(response => response.json())
+
   .then(responseJSON => {return responseJSON.filter(r => r[dataValues[0]])})
   .then(farmersMarkets => dispatch({ type: 'FETCH_FARMERS_MARKETS', payload: farmersMarkets }))
 }
