1a6e5f6b8cd2951dd9de4d49a37ba173cc1fd8cc
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-14 12:09:58 -0500

    filter multiple days at once

--- notes.js
+++ notes.js
@@ -1,1 +1,3 @@
 .then(responseJSON => responseJSON.filter(r => {for(var i=0; i < dataValues.length; i++) {return r[dataValues[i]]}}))
+
+.then(responseJSON => responseJSON.filter(r => {for(var i=0; i < dataValues.length; i++) {if (r.hasOwnProperty(dataValues[i])) {filteredMarkets.push(r)}}}))
--- src/actions/filterFetchFarmersMarkets.js
+++ src/actions/filterFetchFarmersMarkets.js
@@ -4,21 +4,29 @@ export function filterFetchFarmersMarkets(data) {
   let dataValues = Object.values(data)
   dataValues.shift()
   let filteredMarkets = []
-  if (data.search !== "") {
+  if (data.search !== "" && dataValues.length > 0) {
   return (dispatch) => {
 	  dispatch({ type: 'LOADING_MARKETS' })
   return fetch(`https://data.cityofnewyork.us/resource/94pk-v63f.json?zipcode=${data.search}`)
   .then(response => response.json())
-  // .then(responseJSON => {return responseJSON})
-  .then(responseJSON => {if(dataValues.length > 0) {return responseJSON.filter(r => r[dataValues[0]])} else {return responseJSON}})
+  .then(responseJSON => responseJSON.filter(r => {for(var i=0; i < dataValues.length; i++) {return r[dataValues[i]]}}))
   .then(farmersMarkets => dispatch({ type: 'FETCH_FARMERS_MARKETS', payload: farmersMarkets }))
 }
-} else {
+} else if (data.search !== "" && dataValues.length <= 0){
+  return (dispatch) => {
+	  dispatch({ type: 'LOADING_MARKETS' })
+  return fetch(`https://data.cityofnewyork.us/resource/94pk-v63f.json?zipcode=${data.search}`)
+  .then(response => response.json())
+  .then(farmersMarkets => dispatch({ type: 'FETCH_FARMERS_MARKETS', payload: farmersMarkets }))
+}
+}
+else {
   return (dispatch) => {
 	  dispatch({ type: 'LOADING_MARKETS' })
   return fetch(`https://data.cityofnewyork.us/resource/94pk-v63f.json`)
   .then(response => response.json())
-  .then(responseJSON => responseJSON.filter(r => {for(var i=0; i < dataValues.length; i++) {return r[dataValues[i]]}}))
+  .then(responseJSON => responseJSON.filter(r => {for(var i=0; i < dataValues.length; i++) {if (r.hasOwnProperty(dataValues[i])) {return filteredMarkets.push(r)}}}))
+  .then(console.log(filteredMarkets.filter((obj, key, array) => array.map((obj2) => obj.id !== obj2.id))))
   .then(farmersMarkets => dispatch({ type: 'FETCH_FARMERS_MARKETS', payload: farmersMarkets }))
 }
 }
