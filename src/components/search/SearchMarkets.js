5dae704a9ebd972eccf4d1582b98c4b512c9d85e
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-15 10:49:48 -0500

    only render zip and days if they are searched

--- src/components/search/SearchMarkets.js
+++ src/components/search/SearchMarkets.js
@@ -1,9 +1,53 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 
-const SearchMarkets = ({searchParams}) =>
-    <div>
-    <div className="searchResults">Searched for Markets Open On: {searchParams.searchParams.map(param => param.charAt(0).toUpperCase() + param.slice(1)).join(", ")}</div>
-    <div className="searchResults">Within the Zip Code: {searchParams.zip}</div>
-    </div>
+// const SearchMarkets = ({searchParams}) =>
+//     <div>
+//     <div className="searchResults">Searched for Markets Open On: {searchParams.searchParams.map(param => param.charAt(0).toUpperCase() + param.slice(1)).join(", ")}</div>
+//     <div className="searchResults">Within the Zip Code: {searchParams.zip}</div>
+//     </div>
+
+
+class SearchMarkets extends Component {
+
+  constructor(props) {
+    super(props);
+    if (props.searchParams.searchParams !== [] && props.searchParams.zip !== ""){
+    this.state = {
+      searchParams: props.searchParams.searchParams,
+      zip: props.searchParams.zip
+    }
+  } else if (props.searchParams.searchParams !== [] && props.searchParams.zip === "") {
+    this.state = {
+      searchParams: props.searchParams.searchParams,
+      zip: false
+    }
+  } else if (props.searchParams.searchParams === [] && props.searchParams.zip !== "") {
+    this.state = {
+      searchParams: false,
+      zip: props.searchParams.zip
+    }
+  }
+  }
+
+  renderZip = () => {
+    if (this.state.zip !== false) {
+      return "Within the Zip Code: " + this.state.zip
+    } else {
+      return <div></div>
+    }
+  }
+
+  render () {
+
+    return (
+      <div>
+        <div className="searchResults">{this.state.searchParams.map(param => "Searched for Markets Open On: " + param.charAt(0).toUpperCase() + param.slice(1)).join(", ")}</div>
+        <div className="searchResults">{this.renderZip()}</div>
+      </div>
+    )
+  }
+
+}
 
 export default SearchMarkets
