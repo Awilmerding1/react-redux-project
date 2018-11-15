8294b9f04c7b4d4a2ba3cb65c875386e43b92322
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-15 10:09:15 -0500

    made farmers market stateless

--- src/components/farmers_markets/FarmersMarket.js
+++ src/components/farmers_markets/FarmersMarket.js
@@ -2,21 +2,14 @@
 import React, { Component } from 'react';
 import GroceryListsContainer from '../../containers/GroceryListsContainer'
 
-class FarmersMarket extends Component {
+const FarmersMarket = ({farmersMarket}) =>
+  <div className= "marketSearchandList">
+    <div></div>
+    <li  className="marketListItem">
+      {farmersMarket.facilityname} - {farmersMarket.address}, {farmersMarket.borough} {farmersMarket.zipcode}<br/>
+      <GroceryListsContainer farmersMarket={farmersMarket} />
+    </li><br/><br/>
+  </div>
 
-  render() {
-    const { farmersMarket } = this.props;
-
-    return (
-      <div className= "marketSearchandList">
-        <div></div>
-        <li onClick={this.handleOnClick} className="marketListItem">
-          {farmersMarket.facilityname} - {farmersMarket.address}, {farmersMarket.borough} {farmersMarket.zipcode}<br/>
-          <GroceryListsContainer farmersMarket={farmersMarket} />
-        </li><br/><br/>
-      </div>
-    );
-  }
-};
 
 export default FarmersMarket;
