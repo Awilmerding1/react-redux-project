b68fea3b757143ff38eb69b0fab8d6986be3e4dd
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-15 11:05:20 -0500

    groceryItem stateless

--- src/components/grocery_lists/GroceryItem.js
+++ src/components/grocery_lists/GroceryItem.js
@@ -1,26 +1,13 @@
 
 import React, { Component } from 'react';
 
-class GroceryItem extends Component {
-
-  handleOnClick = () => {
-    this.props.deleteGroceryItem(this.props.groceryItem.id);
-  }
-
-  render() {
-    const { groceryItem } = this.props
-
-    return (
-      <div>
-        <li className="groceryItem">
-          {groceryItem.text}
-          <button className="groceryButton" onClick={this.handleOnClick}> Delete </button>
-        </li>
-
-      </div>
-    );
-  }
-
-};
+const GroceryItem = ({groceryItem, deleteGroceryItem }) =>
+  <div>
+    <li className="groceryItem">
+      {groceryItem.text}
+      <button className="groceryButton" onClick={() =>
+       deleteGroceryItem(groceryItem.id)}> Delete </button>
+    </li>
+  </div>
 
 export default GroceryItem;
--- src/components/search/SearchMarkets.js
+++ src/components/search/SearchMarkets.js
@@ -1,13 +1,6 @@
 // import React from 'react';
 import React, { Component } from 'react';
 
-// const SearchMarkets = ({searchParams}) =>
-//     <div>
-//     <div className="searchResults">Searched for Markets Open On: {searchParams.searchParams.map(param => param.charAt(0).toUpperCase() + param.slice(1)).join(", ")}</div>
-//     <div className="searchResults">Within the Zip Code: {searchParams.zip}</div>
-//     </div>
-
-
 class SearchMarkets extends Component {
 
   constructor(props) {
