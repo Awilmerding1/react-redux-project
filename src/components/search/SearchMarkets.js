4ae734536d96cd3ef4fe8d6689b75c9d78fd7652
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-19 11:41:08 -0500

    search markets render searches inline

--- src/components/search/SearchMarkets.js
+++ src/components/search/SearchMarkets.js
@@ -3,35 +3,12 @@ import React, { Component } from 'react';
 
 class SearchMarkets extends Component {
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     searchParams: this.props.searchParams.searchParams,
-  //     zip: this.props.searchParams.zip
-  //   }
-  // }
-
-  renderZip = () => {
-    if (this.props.searchParams.zip !== "") {
-      return "Within the Zip Code: " + this.props.searchParams.zip
-    } else {
-      return <div></div>
-    }
-  }
-
-  renderDays = () => {
-    if (this.props.searchParams.searchParams !== []) {
-      return "Searched for Markets Open On: " + this.props.searchParams.searchParams.map(p => p.charAt(0).toUpperCase() + p.slice(1)).join(", ")
-    } else {
-      return <div></div>
-    }
-  }
 
   render () {
     return (
       <div>
-        <div className="searchResults">{this.renderDays()}</div>
-        <div className="searchResults">{this.renderZip()}</div>
+        <div className="searchResults">{this.props.searchParams.searchParams.length > 0 ? "Searched for Markets Open On: " + this.props.searchParams.searchParams.map(p => p.charAt(0).toUpperCase() + p.slice(1)).join(", ") : <p></p>}</div>
+        <div className="searchResults">{this.props.searchParams.zip !== "" ? "Within the Zip Code: " + this.props.searchParams.zip : <p></p>}</div>
       </div>
     )
   }
