930c4e0c967230633b0cf522f39d0a7ddfeccd57
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-26 14:03:50 -0500

    only show market name on click

--- src/components/grocery_lists/CombinedGroceryList.js
+++ src/components/grocery_lists/CombinedGroceryList.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-const CombinedGroceryList = ({markets }) => {
+class CombinedGroceryList extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markets: this.marketsAndItems(),
+      clicked: false
+    }
+  }
 
-  const marketItemArrays = markets.map(market => market.items).flat()
-  const items = marketItemArrays.map(item => item.text)
+  marketsAndItems = () => {
+  let items = this.props.markets.map(market => market.items).flat()
+  let matchItems = this.props.markets.filter(market => market.id === (items.map(item => item.marketId)))
+  let marketItems = []
+  this.props.markets.map(market => marketItems.push({facilityname: market.facilityname, items: market.items}))
+  return marketItems
+  }
+
+  clicked = () => {
+    const { clicked } = this.state.clicked;
+    this.setState({clicked: !clicked})
+  }
+
+render () {
 
 return (
   <div>
-    {items.map(item => <li key={cuidFn()}>{item}</li>)}
+    <div className="showMarketNames" onClick={this.clicked}>Show Market Names</div>
+    <div>{ this.state.markets.map(market => <li>{market.items.map(item => item.text)  + (this.state.clicked ? " " :  "- " + market.facilityname)}</li>)}</div>
   </div>
 
 )
+}
 }
 export default CombinedGroceryList;
