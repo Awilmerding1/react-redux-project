7aead61110cad19098dae1a400697557aba99259
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-26 11:17:48 -0500

    days and times listed

--- src/components/farmers_markets/FarmersMarket.js
+++ src/components/farmers_markets/FarmersMarket.js
@@ -5,14 +5,12 @@ import GroceryListsContainer from '../../containers/GroceryListsContainer'
 const FarmersMarket = ({farmersMarket}) => {
 
   const days = Object.keys(farmersMarket).filter(key => key === "monday" || key === "tuesday" || key === "wednesday" || key === "thursday" || key === "friday" || key === "saturday" || key === "sunday")
-  const times = Object.values(farmersMarket).filter(key => key === "monday" || key === "tuesday" || key === "wednesday" || key === "thursday" || key === "friday" || key === "saturday" || key === "sunday")
-
 
   return (
   <div className= "marketSearchandList">
     <div></div>
     <li  className="marketListItem">
-      {farmersMarket.facilityname} - {farmersMarket.address}, {farmersMarket.borough} {farmersMarket.zipcode} <p>{days.map(day => day.charAt(0).toUpperCase() + day.slice(1)).join(", ")} : {times.map(time => time)}</p><br/>
+      {farmersMarket.facilityname} - {farmersMarket.address}, {farmersMarket.borough} {farmersMarket.zipcode} <p>{days.map(day => <p className="daysOpen">{day.charAt(0).toUpperCase() + day.slice(1) + ": " + farmersMarket[day]}</p>)}</p><br/>
       <GroceryListsContainer farmersMarket={farmersMarket} />
     </li><br/><br/>
   </div>
